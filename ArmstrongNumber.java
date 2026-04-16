825e21c3c1710e216a0d750542c97eecc9d49c7f
sanjeev <Sanjeevsanju4000@gmail.com>
2026-04-16 23:52:39 +0530

    Update ArmstrongNumber.java
    small change in algorithm

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArmstrongNumber {
+public class ArmstrongNumber { //read 
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
