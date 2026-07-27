40d3ee8a610823345b66325d3ea6a81024003044
sanjeev7007 <sanjeevsanju4000@gmail.com>
2026-07-28 01:39:14 +0530

    new practice

--- classpractice.java
+++ classpractice.java
@@ -1,14 +1,29 @@
-class Addition{
+/**class Addition{
     public int add(int a , int b)
     {
         return a + b;
     }
 }
 public class classpractice{
-    public static void main(String a[]){
+    public static void main(String [] args){
 
         Addition add = new Addition();
         int result = add.add(8,9);
         System.out.println(result);
     }
+}*/
+
+/**class Bus{
+    public int horn(){
+        System.out.print("I am adding");
+        return 0;
+
+    }
 }
+
+public class classpractice {
+    public static void main (String [] a){
+        Bus beem = new Bus();
+        beem.horn();  @Nemosan4318R
+    }
+}*/
