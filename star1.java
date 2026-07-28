860378f0f94ff194736392e35b237e9d3c14c0ed
sanjeev7007 <sanjeevsanju4000@gmail.com>
2026-07-28 22:42:53 +0530

    Star pattern

--- star1.java
+++ star1.java
@@ -1,11 +1,10 @@
 class star1{
     public static void main(String args[]){
         int star[][]=new int [5][];
-        star [0] = new int [1];
-        star [1] = new int [2];
-        star [2] = new int [3];
-        star [3] = new int [4];
-        star [4] = new int [5];
+        for (int i=0;i<=4;i++){
+                star [i] = new int [i+1];   
+        }
+        
         for (int i = 0 ; i<star.length ; i++){
             for (int j=0 ; j<star[i].length ; j++){
                 System.out.print("*");
